de3511eb47eede36b9be81e1620d30f113b75e73
Daniil Surin <wkingsurin@gmail.com>
2026-09-16 19:02:40 +0500

    feat(orders page): render toolbars

--- app/(dashboard)/orders/[id]/page.tsx
+++ app/(dashboard)/orders/[id]/page.tsx
@@ -1,24 +1,22 @@
-import TopBar from "@/components/dashboard/topbar";
 import OrderDetails from "./details/order-details";
 import Toolbar from "./toolbar/toolbar";
-import OrderDetailsToolbar from "@/features/orders/components/details/toolbar";
 import OrderItemsTable from "@/features/orders/components/details/table";
 import DashboardTable from "@/components/dashboard/dashboard-table";
+import OrderDetailsToolbar from "@/features/orders/components/details/toolbar";
 
 export default function OrderPage() {
   return (
     <div className="flex flex-col gap-3 h-full min-h-0">
-      <TopBar>
-        <OrderDetailsToolbar />
-      </TopBar>
-
       <div className="flex flex-col gap-3 flex-1 min-h-0 overflow-hidden">
         <div className="flex flex-col gap-3 p-3 rounded-md w-full min-h-[260px] bg-white border-[0.5px] border-black/10">
           <Toolbar />
           <OrderDetails />
         </div>
 
-        <DashboardTable className="flex-1 min-h-0">
+        <DashboardTable
+          className="flex-1 min-h-0"
+          toolbar={<OrderDetailsToolbar />}
+        >
           <OrderItemsTable />
         </DashboardTable>
       </div>
--- app/(dashboard)/orders/client.tsx
+++ app/(dashboard)/orders/client.tsx
@@ -1,20 +1,16 @@
 "use client";
 
 import DashboardTable from "@/components/dashboard/dashboard-table";
-import TopBar from "@/components/dashboard/topbar";
 import OrdersTable from "@/features/orders/components/table";
 import OrdersToolbar from "@/features/orders/components/toolbar";
 
 export default function OrdersClient() {
   return (
-    <>
-      <TopBar>
-        <OrdersToolbar />
-      </TopBar>
-
-      <DashboardTable className="h-[calc(100dvh-30px-16px-12px-12px)]">
-        <OrdersTable />
-      </DashboardTable>
-    </>
+    <DashboardTable
+      toolbar={<OrdersToolbar />}
+      className="h-[calc(100dvh-12px-12px)]"
+    >
+      <OrdersTable />
+    </DashboardTable>
   );
 }
