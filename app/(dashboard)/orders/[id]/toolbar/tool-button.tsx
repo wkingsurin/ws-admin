d567d1462f2787ba05fba9e0d81a72cb73059cb8
Daniil Surin <wkingsurin@gmail.com>
2026-09-19 13:13:14 +0500

    feat(orders-page): add ui logic to order
    - Add logic: cancel, hide and delete order

--- app/(dashboard)/orders/[id]/toolbar/tool-button.tsx
+++ app/(dashboard)/orders/[id]/toolbar/tool-button.tsx
@@ -1,19 +1,57 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { LucideIcon } from "lucide-react";
+import { ReactNode } from "react";
 
 interface ToolButtonProps {
   icon: LucideIcon;
   label: string;
   style: string;
+  dialog: { title: string; notice: string };
+  onClick: () => void;
 }
 
-export default function ToolButton({ icon, label, style }: ToolButtonProps) {
+export default function ToolButton({
+  icon,
+  label,
+  style,
+  dialog,
+  onClick,
+}: ToolButtonProps) {
   const Icon = icon;
 
   return (
-    <Button className={`h-8 rounded-md px-3 ${style}`}>
-      <Icon className={`size-4 stroke-[1.5px]`} />
-      <span>{label}</span>
-    </Button>
+    <Dialog>
+      <DialogTrigger
+        className={`flex items-center gap-2 h-8 rounded-md px-3 ${style}`}
+      >
+        <Icon className={`size-4 stroke-[1.5px]`} />
+        <span>{label}</span>
+      </DialogTrigger>
+      <DialogContent className="w-full">
+        <DialogHeader>
+          <span className="text-base font-medium">{dialog.title}</span>
+          <p>{dialog.notice}</p>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose
+            render={
+              <Button className="px-3" variant="destructive" onClick={onClick}>
+                Confirm
+              </Button>
+            }
+          />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- app/(dashboard)/orders/[id]/toolbar/toolbar.tsx
+++ app/(dashboard)/orders/[id]/toolbar/toolbar.tsx
@@ -1,28 +1,50 @@
-import { EyeOff, LucideIcon, SquarePen, Trash2 } from "lucide-react";
+"use client";
+
+import { EyeOff, LucideIcon, Trash2, Undo2 } from "lucide-react";
 import ToolButton from "./tool-button";
 
 export default function Toolbar() {
-  const TOOLBAR_BUTTONS: { icon: LucideIcon; label: string; style: string }[] =
-    [
-      {
-        icon: SquarePen,
-        label: "Edit",
-        style:
-          "bg-black/10 border-[0.5px] border-black/5 text-black hover:bg-black/15",
+  const TOOLBAR_BUTTONS: {
+    icon: LucideIcon;
+    label: string;
+    style: string;
+    dialog: { title: string; notice: string };
+    onClick: () => void;
+  }[] = [
+    {
+      icon: Undo2,
+      label: "Cancel",
+      style:
+        "bg-black/10 border-[0.5px] border-black/5 text-black hover:bg-black/15",
+      dialog: {
+        title: "Cancel order?",
+        notice: "You`re sure? This action cannot be undo!",
       },
-      {
-        icon: EyeOff,
-        label: "Hide",
-        style:
-          "bg-black/10 border-[0.5px] border-black/5 text-black hover:bg-black/15",
+      onClick: () => console.log("Order cancelled!"),
+    },
+    {
+      icon: EyeOff,
+      label: "Hide",
+      style:
+        "bg-black/10 border-[0.5px] border-black/5 text-black hover:bg-black/15",
+      dialog: {
+        title: "Hide order?",
+        notice: "You`re sure? This action cannot be undo!",
       },
-      {
-        icon: Trash2,
-        label: "Delete",
-        style:
-          "bg-[#FE414A]/10 border-[0.5px] border-[#FE414A]/5 text-[#FE414A]/75 hover:bg-[#FE414A]/15 hover:text-[#FE414A]",
+      onClick: () => console.log("Order hidden!"),
+    },
+    {
+      icon: Trash2,
+      label: "Delete",
+      style:
+        "bg-[#FE414A]/10 border-[0.5px] border-[#FE414A]/5 text-[#FE414A]/75 hover:bg-[#FE414A]/15 hover:text-[#FE414A]",
+      dialog: {
+        title: "Delete order?",
+        notice: "You`re sure? This action cannot be undo!",
       },
-    ];
+      onClick: () => console.log("Order deleted!"),
+    },
+  ];
 
   return (
     <div className="flex gap-2">
@@ -32,6 +54,8 @@ export default function Toolbar() {
           icon={tool.icon}
           label={tool.label}
           style={tool.style}
+          dialog={tool.dialog}
+          onClick={tool.onClick}
         />
       ))}
     </div>
